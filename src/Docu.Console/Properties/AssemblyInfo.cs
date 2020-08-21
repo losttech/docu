2d551b837d5a168534d8c0f4543f9d2acb1c031b
Victor Milovanov <lost@losttech.software>
2020-08-21 16:45:30 -0700

    switch to the new project format for Console

--- src/Docu.Console/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-[assembly:AssemblyTitle("Docu")]
-[assembly:AssemblyDescription("Simple documentation done simply")]
-[assembly:AssemblyProduct("Docu")]
-[assembly:AssemblyCopyright("Copyright © James Gregory 2009")]
